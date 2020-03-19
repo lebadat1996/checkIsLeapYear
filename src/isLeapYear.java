5862d2d7ac245cdf467e6e7012ec627507ad02a5
badat <you@example.com>
2020-03-19 15:02:24 +0700

    checkIsLeapYear

---
+++ src/isLeapYear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class isLeapYear {
+    public static void main(String[] args) {
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Year:  ");
+        year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d is a leap year", year);
+                } else {
+                    System.out.printf("%d is NOT a leap year", year);
+                }
+            } else {
+                System.out.printf("%d is a leap year", year);
+            }
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
